d4d3d805fb087a9c2369601e7455b585cc3e19e9
jjsch-dev <juanjoseschiavoni@hotmail.com>
2022-11-12 19:42:02 -0300

    add copyright license in files

--- TempSensors.cpp
+++ TempSensors.cpp
@@ -1,9 +1,28 @@
 /**
  * Encapsulates the temperature and humidity sensors.  
- * (C) Juan Schiavoni 2022
- *
  * One read at a time is performed to optimize the time the CPU 
  * hangs while performing the read.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2022 Juan Schiavoni
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */ 
 #include "TempSensors.h"
 
--- filament_dryer_rp2040.ino
+++ filament_dryer_rp2040.ino
@@ -1,9 +1,29 @@
 /**
  * 3D Filament dryer using a Raspberry Pi Pico (RP2040). 
- * (C) Juan Schiavoni 2022
  *
  * Uses a bed heating to remove moisture from the air in order to 
  * prevent filament degradation and improve 3D printing quality.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2022 Juan Schiavoni
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */ 
 #include "TempSensors.h"
 #include "HeaterController.h"
